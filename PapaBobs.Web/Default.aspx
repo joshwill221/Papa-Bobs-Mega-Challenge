a9e1f3d4657dd8667d9c111efca8b0773e216d35
joshwill221 <joshwill221@gmail.com>
2018-10-11 10:57:17 +0800

    Created and styled the front end Order form using bootstrap

--- PapaBobs.Web/Default.aspx
+++ PapaBobs.Web/Default.aspx
@@ -5,14 +5,97 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    <div class="container">
+        <div class="page-header">
+            <h2>Papa Bob's Pizza</h2>
+            <p>Pizza to Code By</p>
+        </div>
 
-        <asp:Button ID="orderButton" Text="Order" runat="server" OnClick="orderButton_Click" />
+        <div class="form-group">
+            <label>Size:</label>
+            <asp:DropDownList ID="sizeDropDown" runat="server" CssClass="form-control">
+                <asp:ListItem Text="Chose One..." Selected="True"></asp:ListItem>
+                <asp:ListItem Text="Small (12 inch - $12)" Value="Small"></asp:ListItem>
+                <asp:ListItem Text="Medium (14 inch - $14)" Value="Medium"></asp:ListItem>
+                <asp:ListItem Text="Large (16 inch - $16)" Value="Large"></asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <label>Crust:</label>
+            <asp:DropDownList ID="crustDropDown" runat="server" CssClass="form-control">
+                <asp:ListItem Text="Choose one..." Selected="True"></asp:ListItem>
+                <asp:ListItem Text="Regular" Value="Regular"></asp:ListItem>
+                <asp:ListItem Text="Thin" Value="Thin"></asp:ListItem>
+                <asp:ListItem Text="Thick (+ $2)" Value="Thick"></asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <div class="checkbox">
+                <label><asp:CheckBox ID="sausageCheckBox" runat="server" />Sausage</label><br />
+                <label><asp:CheckBox ID="pepperoniCheckbox" runat="server" />Pepperoni</label><br />
+                <label><asp:CheckBox ID="onionCheckBox" runat="server" />Onions</label><br />
+                <label><asp:CheckBox ID="greenPepperCheckBox" runat="server" />Green Peppers</label>
+            </div>
+
+        </div>
+
+        <h3>Deliver To:</h3>
+
+        <div class="form-group">
+
+            <label>Name:</label>
+            <asp:TextBox ID="nameTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+            <br />
+            <label>Address:</label>
+            <asp:TextBox ID="addressTextbox" runat="server" CssClass="form-control"></asp:TextBox>
+            <br />
+            <label>Zip Code:</label>
+            <asp:TextBox ID="zipCodeTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+            <br />
+            <label>Phone:</label>
+            <asp:TextBox ID="phoneTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+
+        </div>
+
+        <h3>Payment:</h3>
+
+        <div class="form-group">
+
+            <div class="custom-radio">
+                <label>
+                    <asp:RadioButton ID="cashRadio" runat="server" GroupName="PaymentGroup" />
+                    Cash
+                </label>
+            </div>
+            <div class="custom-radio">
+                <label>
+                    <asp:RadioButton ID="creditRadio" runat="server" GroupName="PaymentGroup" />
+                    Credit
+                </label>
+            </div>
+
+        </div>
+
+        <div class="form-group">
+
+            <asp:Button ID="orderButton" Text="Order" runat="server" OnClick="orderButton_Click" CssClass="btn btn-lg btn-primary" />
+
+        </div>
+
+        <h3>Total Cost:</h3>
+        <div class="form-group">
+
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+
+        </div>
     
     </div>
     </form>
+
+    <script src="scripts/jquery-3.0.0.min.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+
 </body>
 </html>
--- PapaBobs.Web/Default.aspx.designer.cs
+++ PapaBobs.Web/Default.aspx.designer.cs
@@ -22,6 +22,114 @@ namespace PapaBobs.Web {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// sizeDropDown control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList sizeDropDown;
+        
+        /// <summary>
+        /// crustDropDown control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList crustDropDown;
+        
+        /// <summary>
+        /// sausageCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox sausageCheckBox;
+        
+        /// <summary>
+        /// pepperoniCheckbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox pepperoniCheckbox;
+        
+        /// <summary>
+        /// onionCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox onionCheckBox;
+        
+        /// <summary>
+        /// greenPepperCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox greenPepperCheckBox;
+        
+        /// <summary>
+        /// nameTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nameTextBox;
+        
+        /// <summary>
+        /// addressTextbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox addressTextbox;
+        
+        /// <summary>
+        /// zipCodeTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox zipCodeTextBox;
+        
+        /// <summary>
+        /// phoneTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox phoneTextBox;
+        
+        /// <summary>
+        /// cashRadio control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton cashRadio;
+        
+        /// <summary>
+        /// creditRadio control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton creditRadio;
+        
+        /// <summary>
         /// orderButton control.
         /// </summary>
         /// <remarks>
@@ -29,5 +137,14 @@ namespace PapaBobs.Web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button orderButton;
+        
+        /// <summary>
+        /// resultLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label resultLabel;
     }
 }
